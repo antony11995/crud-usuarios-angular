7a97fe932a8baf8202aaf9333e167afc9cf78699
Antony Martinez <antony11995@hotmail.com>
2025-09-19 10:46:16 +0200

    Creación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  
+  { path: 'home', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: 'newuser', component: UserFormComponent },
+  { path: 'updateuser/:id', component: UserFormComponent },  
+  { path: '**', component: NotFoundComponent },
+];
